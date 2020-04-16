bfaaafa4cc96d9131167f818677a0bf1a39756de
Rahul Mitra <rmitra@trincoll.edu>
2020-04-16 00:11:34 -0400

    inserted into employees

--- Database_Insertion.sql
+++ Database_Insertion.sql
@@ -11,4 +11,19 @@ delete from operates;
 delete from contains;
 delete from protected_by;
 delete from ordered_by;
-insert into employee values('1234','5678','Rahul Mitra', '21000');
+insert into employee values('23134212','12425678','Rahul Mitra', '21000.00');
+insert into employee values('23324485','84367831','Christopher Armen', '150000.00');
+insert into employee values('87986845','46757256', 'Alisa Levin','30000.00');
+insert into employee values('48937532','43283547', 'Fumihiro Tamada','35000.00');
+insert into employee values('21389845','10909409', 'Skyler Szot','36000.99');
+insert into employee values('90238294','01248238', 'Hunter Moore','49000.00');
+insert into employee values('34798349','12294830', 'Logan Drescher','42000.00');
+insert into employee values('90217498','34354712', 'Kevin Huang','75000.12.00');
+insert into employee values('92794862','35837254', 'Digesh Chitrakar','29999.99');
+insert into employee values('11273986','17985248', 'Divas Subedi','32000.00');
+insert into employee values('12829743','24386632', 'Andrew Clark','100000.00');
+insert into employee values('24378463','21449538', 'Edwin Aldrich','50000.00');
+select * from employee
+
+
+
