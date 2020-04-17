3c4c5bb28c4f55bab9cd7cefe74b6fc05bbdc515
Rahul Mitra <rmitra@trincoll.edu>
2020-04-17 00:41:46 -0400

    added third query

--- Database_Query.sql
+++ Database_Query.sql
@@ -11,4 +11,14 @@ where E.salary >  (select employee.salary
                   from employee,warehouse
                   where warehouse.city = 'Seattle'
                   and   employee.warehouse_ID = warehouse.warehouse_ID);
+
+-- Q3. This query lists the dependents by ID,name and age for all employees who don't work in a warehouse in Seattle.
+-- This query uses subqueries in a non-trivial manner and potentially uses four or more relations ? 
+select dependent.d_ID, dependent.name
+from dependent, employee
+where dependent.employee_ID = employee.employee_ID
+except
+select dependent.d_ID, dependent.name, employee.name
+from dependent join employee using (employee_ID)
+where employee.warehouse_ID in (select warehouse_ID from warehouse where city = 'Seattle');
                     
