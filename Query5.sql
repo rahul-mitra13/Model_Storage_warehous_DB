86af8f3afc4182e2d73acda1916bd756e22fe74b
Rahul Mitra <rmitra@trincoll.edu>
2020-04-23 16:49:41 -0400

    Added new comment

--- Query5.sql
+++ Query5.sql
@@ -1,5 +1,5 @@
 -- Q5. This query lists all machines by name and ID (and the employees who use them) that are used by employees who earn more than the avergae salary of all employees.
--- (This query uses set operations and subqueries in a non-trivial manner. This query also uses four or more relations. Notice Chris Armen uses three machines while Kevin Huang uses none.)
+-- (This query uses set operations(in particular, testing membership) and subqueries in a non-trivial manner. This query also uses four or more relations. Notice Chris Armen uses three machines while Kevin Huang uses none.)
 select machines.machine_ID, machines.name, employee.name
 from machines join operates using(machine_ID), employee
 where operates.employee_ID = employee.employee_ID and
