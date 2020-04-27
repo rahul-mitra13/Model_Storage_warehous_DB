ed53335e5fd913f65143adb63808eea0e5ce28e4
Rahul Mitra <rmitra@trincoll.edu>
2020-04-27 19:57:49 -0400

    added some stuff

--- Query2.sql
+++ Query2.sql
@@ -1,4 +1,5 @@
--- Q2. This query lists all employees by name and ID who earns more than at least a single employee who works in a warehouse in Seattle. (It uses a subquery in a non-trivial manner and set comparison)
+-- Q2. This query lists all employees by name and ID who earns more than at least a single employee who works in a warehouse in Seattle. 
+-- (It uses a subquery in a non-trivial manner and set comparison)
 select E.name, E.employee_ID, E.salary
 from employee as E
 where E.salary > (select employee.salary
--- Query4.sql
+++ Query4.sql
@@ -1,6 +1,6 @@
---Q4. This query lists all employees by name and ID who earn higher than the average salary of all employees.
+--Q4. This query lists all employees by name, ID and salary who earn higher than the average salary of all employees.
 -- (This query uses grouping (and also having)).
-select employee.name, employee.employee_ID
+select employee.name, employee.employee_ID, employee.salary
 from employee
 group by employee.employee_ID
 having salary > (select avg(salary) from employee);
